3949b351ccdc5f201524a32ddd43c94107d70283
Neha Bharti <neharoy1989@gmail.com>
2023-03-31 14:16:42 +1100

    forecast page implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { ChangeEvent } from "react";
-import Search from "./components/Search"
+import Forecast from './components/Forecast'
+import Search from './components/Search'
+
 import useForecast from './hooks/useForecast'
 
 const App = (): JSX.Element => {
-  const { searchValue, onInputChange } = useForecast()
+  const { forecast, options, term, onOptionSelect, onSubmit, onInputChange } =
+    useForecast()
+
   return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      <Search searchValue={searchValue} onInputChange={onInputChange} />
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 w-full">
+      {forecast ? (
+        <Forecast data={forecast} />
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   )
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,17 @@
-
-import { ChangeEvent } from 'react';
+import { ChangeEvent, ReactNode } from 'react';
+import { optionType } from '../types';
 import Header from './Header'
+import Suggestions from './Suggestions';
 
 type SearchProps = {
-  searchValue: string;
+  term: string;
   onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  options: [];
+  onOptionSelect: (option: optionType) => void
+  onSubmit: () => void
 }
 
-const Search = ({ searchValue, onInputChange }: SearchProps): JSX.Element => {
+const Search = ({ term, onInputChange, options, onOptionSelect, onSubmit }: SearchProps): JSX.Element => {
   return (
     <>
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
@@ -16,15 +20,15 @@ const Search = ({ searchValue, onInputChange }: SearchProps): JSX.Element => {
           <input
             type='text'
             className='px-2 py-1 rounded-l-md border-2 border-white'
-            value={searchValue}
+            value={term}
             onChange={onInputChange}
           />
+          <Suggestions options={options} onSelect={onOptionSelect} />
           <button
-            // onClick={onSubmit}
-            className='rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer'>
+            onClick={onSubmit}
+            className='rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer'>
             Search
           </button>
-
         </section>
       </section>
     </>
