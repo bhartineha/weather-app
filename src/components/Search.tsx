b1b833947b97efbfd9d7986e6798a80b9015713d
Neha Bharti <neharoy1989@gmail.com>
2024-01-24 14:57:16 +1100

    fixed css

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -14,7 +14,7 @@ type SearchProps = {
 const Search = ({ term, onInputChange, options, onOptionSelect, onSubmit }: SearchProps): JSX.Element => {
   return (
     <>
-      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls mt-60 mb-64 rounded drop-shadow-lg text-zinc-700">
         <Header />
         <section className='relative mt-4'>
           <input
